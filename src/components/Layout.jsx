efe8ef56012175188a9765c3ac4f0fa95d44aba6
Pavan Teja Reddy <142150313+PavanTejaReddy1@users.noreply.github.com>
2026-01-28 16:20:42 +0530

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,1 +1,16 @@
+import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
 
+function Layout() {
+  return (
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+      </ul>
+      <Outlet />
+    </div>
+  );
+}
+
+export default Layout;
